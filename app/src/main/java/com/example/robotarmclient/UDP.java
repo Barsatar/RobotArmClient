a883a24238461a52489df78e8090fb61af7606bd
Barsatar <mmaxx2001@mail.ru>
2023-02-03 19:47:06 +0300

    UDP клиент.

--- app/src/main/java/com/example/robotarmclient/UDP.java
+++ app/src/main/java/com/example/robotarmclient/UDP.java
@@ -5,8 +5,9 @@ import java.net.*;
 
 public class UDP implements Runnable {
     private DatagramSocket UDPsocket;
-    private int PORT;
-    private String IP;
+    private final int PORT;
+    private final String IP;
+    InetAddress IPAddress;
 
     public UDP(String ip, int port) {
         PORT = port;
@@ -25,11 +26,53 @@ public class UDP implements Runnable {
             throw new RuntimeException(e);
         }
 
-        InetAddress IPAddress = null;
         try {
             IPAddress = InetAddress.getByName(this.IP);
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
         }
+
+        this.listen();
+    }
+
+    private void listen() {
+        while (true) {
+            sendMessage("Start recording.");
+            String message = new String(receiveMessage());
+
+            if (message.equals("OK.")) {
+                while (true) {
+                    receiveMessage();
+                }
+            }
+        }
+
+    }
+
+    private void sendMessage(String message) {
+        byte[] sendData = message.getBytes();
+        DatagramPacket send_packet = new DatagramPacket(sendData, message.length(), this.IPAddress, this.PORT);
+
+        try {
+            UDPsocket.send(send_packet);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private byte[] receiveMessage() {
+        byte[] receiveData = new byte[50000];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        try {
+            UDPsocket.receive(receivePacket);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String message = new String(receivePacket.getData());
+        System.out.println(message);
+
+        return receivePacket.getData();
     }
 }
