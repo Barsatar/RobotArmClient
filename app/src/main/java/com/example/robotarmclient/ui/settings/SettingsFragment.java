a99f5690680eebadf7286e9bc1e198a2a7550bf7
Barsatar <mmaxx2001@mail.ru>
2023-02-14 13:55:11 +0300

    Добавление полей настройки TCP подключения в ui.

--- app/src/main/java/com/example/robotarmclient/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/robotarmclient/ui/settings/SettingsFragment.java
@@ -11,19 +11,16 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.robotarmclient.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
-
     private FragmentSettingsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SettingsViewModel settingsViewModel =
-                new ViewModelProvider(this).get(SettingsViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        SettingsViewModel settingsViewModel = new ViewModelProvider(this).get(SettingsViewModel.class);
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSettings;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textViewSettingsTCPSocketTitle = binding.textSettingsTcpSocketTitle;
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), textViewSettingsTCPSocketTitle::setText);
         return root;
     }
 
